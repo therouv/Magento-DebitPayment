4084d9862be5b4ca1b277df1184b7783f273f70f
therouv <therouv@googlemail.com>
2011-06-04 14:22:17 +0200

    [~TASK] Added file and function docs

--- src/app/code/community/Mage/Debit/Model/System/Config/Source/Customer/Group.php
+++ src/app/code/community/Mage/Debit/Model/System/Config/Source/Customer/Group.php
@@ -1,9 +1,30 @@
 <?php
-
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @package    Mage_Debit
+ * @copyright  2011 ITABS GmbH / Rouven Alexander Rieker (http://www.itabs.de)
+ * @copyright  2010 Phoenix Medien GmbH & Co. KG (http://www.phoenix-medien.de)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class Mage_Debit_Model_System_Config_Source_Customer_Group
 {
+	/** 
+     * @var array Customer Groups
+     */
     protected $_options;
 
+    /**
+     * Returns the customer groups as an array for system configuration
+     * 
+     * @return array Customer Groups
+     */
     public function toOptionArray()
     {
         if (!$this->_options) {
