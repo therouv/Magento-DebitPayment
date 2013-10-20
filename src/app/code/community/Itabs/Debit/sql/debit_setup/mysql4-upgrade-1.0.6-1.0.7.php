7bcec514dd641023e3a5f71fd43ffa8cf79ff8de
Rouven Alexander Rieker <therouv@googlemail.com>
2013-10-20 20:28:20 +0200

    [FEATURE] Added database table and models for direct debit mandates

---
+++ src/app/code/community/Itabs/Debit/sql/debit_setup/mysql4-upgrade-1.0.6-1.0.7.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * This file is part of the Itabs_Debit module.
+ *
+ * PHP version 5
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category  Itabs
+ * @package   Itabs_Debit
+ * @author    Rouven Alexander Rieker <rouven.rieker@itabs.de>
+ * @copyright 2008-2013 ITABS GmbH / Rouven Alexander Rieker (http://www.itabs.de)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @version   1.0.2
+ * @link      http://www.magentocommerce.com/magento-connect/debitpayment.html
+ */
+/**
+ * Setup script
+ *
+ * @category  Itabs
+ * @package   Itabs_Debit
+ * @author    Rouven Alexander Rieker <rouven.rieker@itabs.de>
+ * @copyright 2008-2013 ITABS GmbH / Rouven Alexander Rieker (http://www.itabs.de)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @version   1.0.2
+ * @link      http://www.magentocommerce.com/magento-connect/debitpayment.html
+ */
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+$installer->startSetup();
+
+$tableName = $installer->getTable('debit/mandates');
+if ($installer->getConnection()->isTableExists($tableName)) {
+    $installer->getConnection()->dropTable($tableName);
+}
+
+$table = $installer->getConnection()->newTable($tableName)
+    ->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'identity'  => true,
+        'unsigned'  => true,
+        'nullable'  => false,
+        'primary'   => true,
+        ), 'ID')
+    ->addColumn('order_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+            'nullable'  => false,
+            ), 'Order ID')
+    ->addColumn('website_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+            'nullable'  => false,
+            ), 'Website ID')
+    ->addColumn('store_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'nullable'  => false,
+        ), 'Store ID')
+    ->addColumn('increment_id', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
+        'nullable'  => false,
+        ), 'Increment ID')
+    ->addColumn('filename', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
+        'nullable'  => false,
+        ), 'Filename')
+    ->setComment('Debit SEPA direct debit mandates');
+$installer->getConnection()->createTable($table);
+
+$installer->endSetup();
