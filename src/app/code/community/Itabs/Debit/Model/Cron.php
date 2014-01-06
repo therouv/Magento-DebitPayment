734d42a6b1234bf49deab63ed7f76d73d72f919c
Rouven Alexander Rieker <therouv@googlemail.com>
2014-01-06 11:55:58 +0100

    [FEATURE] Added email sending process for mandate pdf

--- src/app/code/community/Itabs/Debit/Model/Cron.php
+++ src/app/code/community/Itabs/Debit/Model/Cron.php
@@ -36,12 +36,15 @@ class Itabs_Debit_Model_Cron
      */
     public function generate()
     {
-        $mandates = $this->_getMandates();
+        $mandates = $this->getMandates();
         if (!$mandates->count()) {
             return $this;
         }
 
         foreach ($mandates as $mandate) {
+            /* @var $mandate Itabs_Debit_Model_Mandates */
+
+            /* @var $order Mage_Sales_Model_Order */
             $order = Mage::getModel('sales/order')->load($mandate->getData('order_id'));
 
             // Check if order exists, if not delete the mandate
@@ -50,8 +53,14 @@ class Itabs_Debit_Model_Cron
                 continue;
             }
 
-            $filePath = $this->_generateMandatePdf($mandate, $order);
-            $this->_sendCustomerEmail($mandate, $order, $filePath);
+            try {
+                $filePath = $this->_generateMandatePdf($mandate, $order);
+                $this->_sendCustomerEmail($mandate, $order, $filePath);
+                $mandate->setData('is_generated', 1)->save();
+            } catch (Exception $e) {
+                $mandate->setData('is_generated', 0)->save();
+                continue;
+            }
         }
     }
 
@@ -60,7 +69,7 @@ class Itabs_Debit_Model_Cron
      *
      * @return Itabs_Debit_Model_Resource_Mandates_Collection
      */
-    protected function _getMandates()
+    public function getMandates()
     {
         /* @var $collection Itabs_Debit_Model_Resource_Mandates_Collection */
         $collection = Mage::getResourceModel('debit/mandates_collection');
@@ -70,16 +79,57 @@ class Itabs_Debit_Model_Cron
     }
 
     /**
-     * @param Itabs_Debit_Model_Mandates $mandate
-     * @param Mage_Sales_Model_Order     $order
-     * @param string                     $filePath
+     * Send the customer email with the mandate pdf
+     *
+     * @param Itabs_Debit_Model_Mandates $mandate  Mandate Model
+     * @param Mage_Sales_Model_Order     $order    Order Model
+     * @param string                     $filePath Path to mandate pdf
      */
     protected function _sendCustomerEmail($mandate, $order, $filePath)
     {
-        // @todo: Implement email sending process
+        /* @var $payment Itabs_Debit_Model_Debit */
+        $payment = $order->getPayment()->getMethodInstance();
+
+        $storeId = $order->getStoreId();
+        $sender = Mage::getStoreConfig('sales_email/order/identity', $storeId);
+        $templateVars = array(
+            'increment_id' => $order->getIncrementId(),
+            'mandate' => $mandate->getData('mandate_reference'),
+            'customer_name' => $payment->getAccountName()
+        );
+
+        // Start store emulation process
+        $appEmulation = Mage::getSingleton('core/app_emulation');
+        $initialEnvironmentInfo = $appEmulation->startEnvironmentEmulation($storeId);
+
+        // Build pdf attachment
+        $mailAttachment = new Zend_Mime_Part(file_get_contents($filePath));
+        $mailAttachment->type = 'application/pdf';
+        $mailAttachment->encoding = Zend_Mime::ENCODING_8BIT;
+        $mailAttachment->disposition = Zend_Mime::DISPOSITION_ATTACHMENT;
+        $mailAttachment->filename = $mandate->getData('mandate_reference').'.pdf';
+        $mailAttachment->charset = '"UTF-8"';
+
+        // Send the transactional email
+        /* @var $mail Mage_Core_Model_Email_Template */
+        $mail = Mage::getModel('core/email_template');
+        $mail->getMail()->addAttachment($mailAttachment);
+        $mail->sendTransactional(
+            Mage::getStoreConfig('debitpayment/sepa/email_template', $storeId),
+            $sender,
+            $payment->getAccountEmail(),
+            $payment->getAccountName(),
+            $templateVars,
+            $storeId
+        );
+
+        // Stop store emulation process
+        $appEmulation->stopEnvironmentEmulation($initialEnvironmentInfo);
     }
 
     /**
+     * Generate the mandate pdf file
+     *
      * @param  Itabs_Debit_Model_Mandates $mandate
      * @param  Mage_Sales_Model_Order     $order
      * @return string
@@ -108,16 +158,18 @@ class Itabs_Debit_Model_Cron
 
         $this->_setFontBold($page, 16);
         $page->drawText($helper->__('SEPA direct debit mandate', 'UTF-8'), $x, $y);
-
         $y -= 30;
+
         $this->_setFontRegular($page, 12);
 
         $page->drawText($helper->__('Payee'), $x, $y, 'UTF-8');
         $page->drawText(Mage::getStoreConfig('debitpayment/bankaccount/account_owner', $order->getStoreId()), $x2, $y, 'UTF-8');
         $y -= 15;
+
         $page->drawText($helper->__('Creditor Identification Number'), $x, $y, 'UTF-8');
         $page->drawText(Mage::getStoreConfig('debitpayment/sepa/creditor_identification_number', $order->getStoreId()), $x2, $y, 'UTF-8');
         $y -= 15;
+
         $page->drawText($helper->__('Mandate Reference'), $x, $y, 'UTF-8');
         $page->drawText($mandate->getData('mandate_reference'), $x2, $y, 'UTF-8');
         $y -= 40;
@@ -215,6 +267,7 @@ class Itabs_Debit_Model_Cron
     {
         $font = Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA);
         $object->setFont($font, $size);
+
         return $font;
     }
 
@@ -229,6 +282,7 @@ class Itabs_Debit_Model_Cron
     {
         $font = Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA_BOLD);
         $object->setFont($font, $size);
+
         return $font;
     }
 }
