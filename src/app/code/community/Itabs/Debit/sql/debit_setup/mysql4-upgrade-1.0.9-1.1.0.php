67754609e0c8cd35cb5942901d7c194c0644997c
Rouven Alexander Rieker <therouv@googlemail.com>
2014-05-24 13:13:29 +0200

    [TASK] Added index to bankdata table

---
+++ src/app/code/community/Itabs/Debit/sql/debit_setup/mysql4-upgrade-1.0.9-1.1.0.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * This file is part of the Itabs_Debit module.
+ *
+ * PHP version 5
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category  Itabs
+ * @package   Itabs_Debit
+ * @author    ITABS GmbH <info@itabs.de>
+ * @copyright 2008-2014 ITABS GmbH (http://www.itabs.de)
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @version   1.0.6
+ * @link      http://www.magentocommerce.com/magento-connect/debitpayment.html
+ */
+/**
+ * Setup script
+ */
+
+/* @var $installer Mage_Eav_Model_Entity_Setup */
+$installer = $this;
+$installer->startSetup();
+
+$installer->getConnection()->addIndex(
+    $installer->getTable('debit/bankdata'),
+    $installer->getIdxName('debit/bankdata', array('swift_code')),
+    array('swift_code')
+);
+
+$installer->getConnection()->addIndex(
+    $installer->getTable('debit/bankdata'),
+    $installer->getIdxName('debit/bankdata', array('routing_number')),
+    array('routing_number')
+);
+
+$installer->endSetup();
